3fdf75d8c6c9bbecbc753e46b02e4dac40eb7e7c
redspor <121644986+redspor@users.noreply.github.com>
2023-03-12 23:26:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,20 @@ def index(m3u8):
     source = source.replace('%3F', '?')
     videoid = request.args.get("videoid")
     '''source = source.replace(videoid+'.m3u8',videoid)'''
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "tr-TR, tr;q = 0.9",
+        "origin": "https://www.maltinok.com",
+        "referer": "https://www.maltinok.com/",
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://king-prawn-app-mx7i9.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
@@ -28,6 +42,20 @@ def getm3u8():
     source = source.replace('https://king-prawn-app-mx7i9.ondigitalocean.app/getm3u8?source=', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "tr-TR, tr;q = 0.9",
+        "origin": "https://www.maltinok.com",
+        "referer": "https://www.maltinok.com/",
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://king-prawn-app-mx7i9.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
@@ -60,7 +88,7 @@ def getstream():
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://1xlite-614716.top/cinema",json=veriler)
+        r = requests.post("https://lite-1x202983.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
             veri = re.findall('"URL":"(.*?)"',veri)
