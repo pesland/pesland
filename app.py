d37326308258316db990eb0ec7d6b352f44e6283
redspor <121644986+redspor@users.noreply.github.com>
2023-03-12 23:22:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import requests
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-import re
 app = Flask(__name__)
 CORS(app)
 @app.route('/<m3u8>')
@@ -14,20 +13,6 @@ def index(m3u8):
     source = source.replace('%3F', '?')
     videoid = request.args.get("videoid")
     '''source = source.replace(videoid+'.m3u8',videoid)'''
-    headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://king-prawn-app-mx7i9.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
@@ -43,20 +28,6 @@ def getm3u8():
     source = source.replace('https://king-prawn-app-mx7i9.ondigitalocean.app/getm3u8?source=', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
-    headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://king-prawn-app-mx7i9.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
