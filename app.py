07082259820a564ceb1b5fff37a681914adfb158
pesland <127784800+pesland@users.noreply.github.com>
2023-03-14 15:30:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,30 +37,7 @@ def index(m3u8):
         tsal = tsal.replace('\n'+'media','\n'+'https://monkfish-app-qpbmn.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/media')
     return tsal
 
-@app.route('/getm3u8',methods=['GET'])
-def getm3u8():
-    source = request.url
-    source = source.replace('https://monkfish-app-qpbmn.ondigitalocean.app/getm3u8?source=', '')
-    source = source.replace('%2F', '/')
-    source = source.replace('%3F', '?')
-    headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
-    ts = requests.get(source, headers=headers)
-    tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://monkfish-app-qpbmn.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
-    return tsal
+
 
 @app.route('/getstream',methods=['GET'])
 def getstream():
@@ -104,7 +81,14 @@ def getstream():
             veri = veri.replace('edge7', 'edge10')
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
-          
+           if "m3u8" in veri:
+                '''return "https://monkfish-app-qpbmn.ondigitalocean.app/getm3u8?source="+veri+'&videoid='+videoid'''
+                return "https://monkfish-app-qpbmn.ondigitalocean.app/"+veri+'&videoid='+videoid
+        else:
+            return "Veri yok"
+
+if __name__ == '__main__':
+    app.run()
 
 if __name__ == '__main__':
     app.run()
