a2dfbcf281ff931259e1bf3a7626801ee9445243
pesland <127784800+pesland@users.noreply.github.com>
2023-04-30 12:07:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,24 @@ CORS(app)
 @app.route('/<m3u8>')
 def index(m3u8):
     m3u8 = request.url.replace('__', '/')
-    source = m3u8.replace('https://cdn.esraerol.workers.dev/', '')
+    source = m3u8.replace('https://plankton-app-ljeqg.ondigitalocean.app/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get('videoid')
     headers = {
         'accept': '*/*',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'tr-TR, tr;q=0.9',
-        'origin': 'https://www.maltinok.com',
-        'referer': 'https://www.maltinok.com/',
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
+https://pingstream.herokuapp.com/
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     ts = requests.get(source, headers=headers)
-    tsal = ts.text.replace(videoid+'_', f'https://volestreamredirtectts.herokuapp.com/https://cdn.esraerol.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
+    tsal = ts.text.replace(videoid+'_', f'https://cdn.esraerol.workers.dev/https://plankton-app-ljeqg.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
     if 'internal' in tsal:
-        tsal = tsal.replace('internal', f'https://volestreamredirtectts.herokuapp.com/https://cdn.esraerol.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
+        tsal = tsal.replace('internal', f'https://cdn.esraerol.workers.dev/https://plankton-app-ljeqg.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
     if 'segment' in tsal:
-        tsal = tsal.replace('\nmedia', f'\nhttps://volestreamredirtectts.herokuapp.com/https://cdn.esraerol.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
+        tsal = tsal.replace('\nmedia', f'\nhttps://cdn.esraerol.workers.dev/https://plankton-app-ljeqg.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
     return tsal
 
 @app.route('/getm3u8', methods=['GET'])
@@ -56,7 +50,7 @@ def getm3u8():
     }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://cdn.esraerol.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    tsal = tsal.replace(videoid+'_','https://plankton-app-ljeqg.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
  
 @app.route('/getstream',methods=['GET'])
@@ -64,7 +58,7 @@ def getstream():
     param = request.args.get("param")
     if param == "getts":
         source = request.url
-        source = source.replace('https://cdn.esraerol.workers.dev/getstream?param=getts&source=','')
+        source = source.replace('https://plankton-app-ljeqg.ondigitalocean.app/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
@@ -102,8 +96,8 @@ def getstream():
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
             if "m3u8" in veri:
-                '''return "https://cdn.esraerol.workers.dev/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://cdn.esraerol.workers.dev/"+veri+'&videoid='+videoid
+                '''return "https://plankton-app-ljeqg.ondigitalocean.app/getm3u8?source="+veri+'&videoid='+videoid'''
+                return "https://plankton-app-ljeqg.ondigitalocean.app/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
  
