6c12b9a53ece1f93a73644cd701ce6e64afa12ea
pesland <127784800+pesland@users.noreply.github.com>
2023-04-30 14:15:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-import asyncio
-import aiohttp
+import requests
 from flask import Flask, request
 from flask_cors import CORS
+import re
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/<m3u8>')
 def index(m3u8):
-    m3u8 = request.url.replace('__','/')
-    source = m3u8
-    source = source.replace('https://volestream.herokuapp.com/', '')
+    m3u8 = request.url.replace('__', '/')
+    source = m3u8.replace('https://volestream.herokuapp.com/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
-    videoid = request.args.get("videoid")
-
+    videoid = request.args.get('videoid')
     headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'tr-TR, tr;q=0.9',
+        'origin': 'https://www.maltinok.com',
+        'referer': 'https://www.maltinok.com/',
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -29,41 +27,25 @@ def index(m3u8):
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-
-    async def fetch(session, url):
-        async with session.get(url, headers=headers) as response:
-            return await response.text()
-
-    async def main():
-        async with aiohttp.ClientSession() as session:
-            ts = await fetch(session, source)
-            tsal = ts.replace(videoid+'_','https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
-            if "internal" in tsal:
-                tsal = tsal.replace('internal','https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/internal')
-            if "segment" in tsal:
-                tsal = tsal.replace('\n'+'media','\n'+'https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/media')
-            tsal = tsal.replace('\n','')
-            tsal = tsal.replace(' ','')
-            return tsal
-
-    tsal = asyncio.run(main())
-
+    ts = requests.get(source, headers=headers)
+    tsal = ts.text.replace(videoid+'_', f'https://pingstream.herokuapp.com/https://volestreamredirtectts.herokuapp.com/https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
+    if 'internal' in tsal:
+        tsal = tsal.replace('internal', f'https://pingstream.herokuapp.com/https://volestreamredirtectts.herokuapp.com/https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
+    if 'segment' in tsal:
+        tsal = tsal.replace('\nmedia', f'\nhttps://pingstream.herokuapp.com/https://volestreamredirtectts.herokuapp.com/https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
     return tsal
 
-
-
-@app.route('/getm3u8',methods=['GET'])
+@app.route('/getm3u8', methods=['GET'])
 def getm3u8():
-    source = request.url
-    source = source.replace('https://volestream.herokuapp.com/getm3u8?source=', '')
+    source = request.args.get('source')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'tr-TR, tr;q=0.9',
+        'origin': 'https://www.maltinok.com',
+        'referer': 'https://www.maltinok.com/',
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -74,9 +56,9 @@ def getm3u8():
     }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    tsal = tsal.replace(videoid+'_','https://pingstream.herokuapp.com/https://volestream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
-
+ 
 @app.route('/getstream',methods=['GET'])
 def getstream():
     param = request.args.get("param")
@@ -121,9 +103,9 @@ def getstream():
             veri = veri.replace('edge100','edge10')
             if "m3u8" in veri:
                 '''return "https://volestream.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://volestream.herokuapp.com/"+veri+'&videoid='+videoid
+                return "https://pingstream.herokuapp.com/https://volestream.herokuapp.com/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
-
+ 
 if __name__ == '__main__':
     app.run()
