2aea417cbed726f8a25b69a19fa09bda2acc2514
pesland <127784800+pesland@users.noreply.github.com>
2023-05-13 11:29:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CORS(app)
 @app.route('/<m3u8>')
 def index(m3u8):
     m3u8 = request.url.replace('__', '/')
-    source = m3u8.replace('https://erdoganladevam.herokuapp.com/', '')
+    source = m3u8.replace('https://cdn.ahgecelersensizeceler.workers.dev/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get('videoid')
@@ -102,8 +102,8 @@ def getstream():
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
             if "m3u8" in veri:
-                '''return "https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
+                '''return "https://cdn.ahgecelersensizeceler.workers.dev/getm3u8?source="+veri+'&videoid='+videoid'''
+                return "https://cdn.ahgecelersensizeceler.workers.dev/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
  
