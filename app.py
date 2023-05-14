df7642842e5de0bdb1e76fc3e5a9aef989c949f1
pesland <127784800+pesland@users.noreply.github.com>
2023-05-14 14:53:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CORS(app)
 @app.route('/<m3u8>')
 def index(m3u8):
     m3u8 = request.url.replace('__', '/')
-    source = m3u8.replace('https://volestream1.com/', '')
+    source = m3u8.replace('https://erdoganladevam.herokuapp.com/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get('videoid')
@@ -56,7 +56,7 @@ def getm3u8():
     }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://volestream1.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    tsal = tsal.replace(videoid+'_','https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
  
 @app.route('/getstream',methods=['GET'])
@@ -64,7 +64,7 @@ def getstream():
     param = request.args.get("param")
     if param == "getts":
         source = request.url
-        source = source.replace('https://volestream1.com/getstream?param=getts&source=','')
+        source = source.replace('https://erdoganladevam.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
@@ -102,8 +102,8 @@ def getstream():
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
             if "m3u8" in veri:
-                '''return "https://volestreamredirect1.ahgecelersensizeceler.workers.dev/https://volestream1.com/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://volestreamredirect1.ahgecelersensizeceler.workers.dev/https://volestream1.com/"+veri+'&videoid='+videoid
+                '''return "https://volestreamredirect1.ahgecelersensizeceler.workers.dev/https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
+                return "https://volestreamredirect1.ahgecelersensizeceler.workers.dev/https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
  
