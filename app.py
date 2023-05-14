cbb447b2188d7f38157999e7988624f109a4291c
pesland <127784800+pesland@users.noreply.github.com>
2023-05-14 22:21:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,13 @@ def index(m3u8):
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
- ts = requests.get(m3u8, headers=headers)
+    ts = requests.get(source, headers=headers)
     tsal = ts.text.replace(videoid+'_', f'https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
     if 'internal' in tsal:
         tsal = tsal.replace('internal', f'https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
     if 'segment' in tsal:
         tsal = tsal.replace('\nmedia', f'\nhttps://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
     return tsal
-
  
 @app.route('/getm3u8', methods=['GET'])
 def getm3u8():
@@ -103,8 +102,8 @@ def getstream():
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
             if "m3u8" in veri:
-                '''return "https://volestreamredirect1.ahgecelersensizeceler.workers.dev/https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://volestreamredirect1.ahgecelersensizeceler.workers.dev/https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
+                '''return "https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
+                return "https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
  
