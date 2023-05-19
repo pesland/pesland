83dd55d4e309327efee310b0045db76a57614a62
pesland <127784800+pesland@users.noreply.github.com>
2023-05-19 22:00:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def getstream():
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://1xlite-9313035.top/cinema",json=veriler)
+        r = requests.post("https://1xlite-502626.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
             veri = re.findall('"URL":"(.*?)"',veri)
