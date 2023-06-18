5e8c5f7f32528822f3169dfab0b530fc9f541f9a
pesland <127784800+pesland@users.noreply.github.com>
2023-06-19 02:18:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,40 @@
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 import re
-import requests
-
+ 
 app = Flask(__name__)
 CORS(app)
-
-m3u8_queue = []
-
-def process_queue():
-    if m3u8_queue:
-        m3u8 = m3u8_queue.pop(0)
-        source = m3u8.replace('https://erdoganladevam.herokuapp.com/', '')
-        source = source.replace('%2F', '/')
-        source = source.replace('%3F', '?')
-        videoid = request.args.get('videoid')
-        headers = {
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'tr-TR, tr;q=0.9',
-            'origin': 'https://www.maltinok.com',
-            'referer': 'https://www.maltinok.com/',
-            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-        }
-      ts = requests.get(source, headers=headers)
+ 
+@app.route('/<m3u8>')
+def index(m3u8):
+    m3u8 = request.url.replace('__', '/')
+    source = m3u8.replace('https://erdoganladevam.herokuapp.com/', '')
+    source = source.replace('%2F', '/')
+    source = source.replace('%3F', '?')
+    videoid = request.args.get('videoid')
+    headers = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'tr-TR, tr;q=0.9',
+        'origin': 'https://www.maltinok.com',
+        'referer': 'https://www.maltinok.com/',
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
+    ts = requests.get(source, headers=headers)
     tsal = ts.text.replace(videoid+'_', f'https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
     if 'internal' in tsal:
         tsal = tsal.replace('internal', f'https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
     if 'segment' in tsal:
         tsal = tsal.replace('\nmedia', f'\nhttps://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
-    return tsal 
-
-@app.route('/<m3u8>')
-def index(m3u8):
-    m3u8 = request.url.replace('__', '/')
-    m3u8_queue.append(m3u8)
-    tsal = process_queue()
     return tsal
-
+ 
 @app.route('/getm3u8', methods=['GET'])
 def getm3u8():
     source = request.args.get('source')
@@ -67,7 +58,7 @@ def getm3u8():
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
-
+ 
 @app.route('/getstream',methods=['GET'])
 def getstream():
     param = request.args.get("param")
@@ -115,6 +106,6 @@ def getstream():
                 return "https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
-
+ 
 if __name__ == '__main__':
     app.run()
