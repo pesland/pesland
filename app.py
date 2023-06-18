efe392e032115bf60776bf99f473a42dc2e67d45
pesland <127784800+pesland@users.noreply.github.com>
2023-06-18 21:15:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,49 @@
-import requests
 from flask import Flask, request
 from flask_cors import CORS
 import re
- 
+import requests
+
 app = Flask(__name__)
 CORS(app)
- 
+
+m3u8_queue = []
+
+def process_queue():
+    if m3u8_queue:
+        m3u8 = m3u8_queue.pop(0)
+        source = m3u8.replace('https://whale-app-25ay3.ondigitalocean.app/', '')
+        source = source.replace('%2F', '/')
+        source = source.replace('%3F', '?')
+        videoid = request.args.get('videoid')
+        headers = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'tr-TR, tr;q=0.9',
+            'origin': 'https://www.maltinok.com',
+            'referer': 'https://www.maltinok.com/',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        }
+        ts = requests.get(source, headers=headers)
+        tsal = ts.text.replace(videoid+'_', f'https://cors-proxy.fringe.zone/https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
+        if 'internal' in tsal:
+            tsal = tsal.replace('internal', f'https://cors-proxy.fringe.zone/https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
+        if 'segment' in tsal:
+            tsal = tsal.replace('\nmedia', f'\nhttps://cors-proxy.fringe.zone/https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
+        return tsal
+
 @app.route('/<m3u8>')
 def index(m3u8):
     m3u8 = request.url.replace('__', '/')
-    source = m3u8.replace('https://whale-app-25ay3.ondigitalocean.app/', '')
-    source = source.replace('%2F', '/')
-    source = source.replace('%3F', '?')
-    videoid = request.args.get('videoid')
-    headers = {
-        'accept': '*/*',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'tr-TR, tr;q=0.9',
-        'origin': 'https://www.maltinok.com',
-        'referer': 'https://www.maltinok.com/',
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
-    ts = requests.get(source, headers=headers)
-    tsal = ts.text.replace(videoid+'_', f'https://cors-proxy.fringe.zone/https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
-    if 'internal' in tsal:
-        tsal = tsal.replace('internal', f'https://cors-proxy.fringe.zone/https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
-    if 'segment' in tsal:
-        tsal = tsal.replace('\nmedia', f'\nhttps://cors-proxy.fringe.zone/https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
+    m3u8_queue.append(m3u8)
+    tsal = process_queue()
     return tsal
- 
+
 @app.route('/getm3u8', methods=['GET'])
 def getm3u8():
     source = request.args.get('source')
@@ -58,7 +67,7 @@ def getm3u8():
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://whale-app-25ay3.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
- 
+
 @app.route('/getstream',methods=['GET'])
 def getstream():
     param = request.args.get("param")
@@ -106,6 +115,6 @@ def getstream():
                 return "https://whale-app-25ay3.ondigitalocean.app/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
- 
+
 if __name__ == '__main__':
     app.run()
