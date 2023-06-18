b44e7fdf479f6e8fb9435b2323d3e2b1a8206ef8
pesland <127784800+pesland@users.noreply.github.com>
2023-06-18 12:31:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index(m3u8):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     ts = requests.get('https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev' + source, headers=headers)
-    tsal = ts.text.replace(videoid+'_', f'https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
+    tsal = ts.text.replace(videoid + '_', f'https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
     if 'internal' in tsal:
         tsal = tsal.replace('internal', f'https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
     if 'segment' in tsal:
@@ -56,17 +56,17 @@ def getm3u8():
     }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    videoid = request.args.get('videoid')
+    tsal = tsal.replace(videoid + '_', f'https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
     return tsal
  
-@app.route('/getstream',methods=['GET'])
+@app.route('/getstream', methods=['GET'])
 def getstream():
     param = request.args.get("param")
     if param == "getts":
-        source = request.url
-        source = source.replace('https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=','')
-        source = source.replace('%2F','/')
-        source = source.replace('%3F','?')
+        source = request.url.replace('https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getstream?param=getts&source=', '')
+        source = source.replace('%2F', '/')
+        source = source.replace('%3F', '?')
         headers = {
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
@@ -81,26 +81,26 @@ def getstream():
             'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
-        ts = requests.get(source,headers=headers)
+        ts = requests.get(source, headers=headers)
         return ts.content
-    if param == "getm3u8":
+    elif param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://1xlite-2023337.top/cinema",json=veriler)
+        r = requests.post("https://1xlite-2023337.top/cinema", json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
-            veri = re.findall('"URL":"(.*?)"',veri)
+            veri = re.findall('"URL":"(.*?)"', veri)
             veri = veri[0].replace("\/", "__")
-            veri = veri.replace('edge3','edge10')
-            veri = veri.replace('edge100','edge10')
-            veri = veri.replace('edge4','edge10')
-            veri = veri.replace('edge2','edge10')
-            veri = veri.replace('edge5','edge10')
-            veri = veri.replace('edge1','edge10')
+            veri = veri.replace('edge3', 'edge10')
+            veri = veri.replace('edge100', 'edge10')
+            veri = veri.replace('edge4', 'edge10')
+            veri = veri.replace('edge2', 'edge10')
+            veri = veri.replace('edge5', 'edge10')
+            veri = veri.replace('edge1', 'edge10')
             veri = veri.replace('edge6', 'edge10')
             veri = veri.replace('edge7', 'edge10')
-            veri = veri.replace(':43434','')
-            veri = veri.replace('edge100','edge10')
+            veri = veri.replace(':43434', '')
+            veri = veri.replace('edge100', 'edge10')
             if "m3u8" in veri:
                 return "https://late-breeze-c588.kkjfjfjsfjsfjsjfj.workers.dev/getm3u8?source=" + veri + "&videoid=" + videoid
         else:
