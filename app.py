0852aea4321c1198af762830f3181a1c6654208f
pesland <127784800+pesland@users.noreply.github.com>
2023-06-19 19:49:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ CORS(app)
  
 @app.route('/<m3u8>')
 def index(m3u8):
-    m3u8 = request.url.replace('__', '/')
+    return "Gerekli işlemleri yapmak için bu URL'i kullanabilirsiniz: /getts?m3u8=" + m3u8
+ 
+@app.route('/getts', methods=['GET'])
+def get_ts():
+    m3u8 = request.args.get('m3u8')
+    m3u8 = m3u8.replace('__', '/')
     source = m3u8.replace('https://erdoganladevam.herokuapp.com/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
@@ -36,7 +41,7 @@ def index(m3u8):
     return tsal
  
 @app.route('/getm3u8', methods=['GET'])
-def getm3u8():
+def get_m3u8():
     source = request.args.get('source')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
@@ -59,11 +64,11 @@ def getm3u8():
     tsal = tsal.replace(videoid+'_','https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
  
-@app.route('/getstream',methods=['GET'])
-def getstream():
+@app.route('/getstream', methods=['GET'])
+def get_stream():
     param = request.args.get("param")
     if param == "getts":
-        source = request.url
+        source = request.args.get("source")
         source = source.replace('https://erdoganladevam.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
@@ -81,31 +86,9 @@ def getstream():
             'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
-        ts = requests.get(source,headers=headers)
+        ts = requests.get(source, headers=headers)
         return ts.content
-    if param == "getm3u8":
-        videoid = request.args.get("videoid")
-        veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://1xlite-2023337.top/cinema",json=veriler)
-        if "FullscreenAllowed" in r.text:
-            veri = r.text
-            veri = re.findall('"URL":"(.*?)"',veri)
-            veri = veri[0].replace("\/", "__")
-            veri = veri.replace('edge3','edge10')
-            veri = veri.replace('edge100','edge10')
-            veri = veri.replace('edge4','edge10')
-            veri = veri.replace('edge2','edge10')
-            veri = veri.replace('edge5','edge10')
-            veri = veri.replace('edge1','edge10')
-            veri = veri.replace('edge6', 'edge10')
-            veri = veri.replace('edge7', 'edge10')
-            veri = veri.replace(':43434','')
-            veri = veri.replace('edge100','edge10')
-            if "m3u8" in veri:
-                '''return "https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
-        else:
-            return "Veri yok"
+    return "Geçersiz parametre"
  
 if __name__ == '__main__':
     app.run()
