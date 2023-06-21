f7ab3e1e2e6f9be8d30f7f35f554ed0441dfef14
pesland <127784800+pesland@users.noreply.github.com>
2023-06-21 17:17:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,14 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 import re
-import datetime;
-
+ 
 app = Flask(__name__)
 CORS(app)
-
+ 
 @app.route('/<m3u8>')
 def index(m3u8):
     m3u8 = request.url.replace('__', '/')
-    now = datetime.datetime.now();
-    source = m3u8.replace('https://hammerhead-app-bcw5r.ondigitalocean.app/', '')
+    source = m3u8.replace('https://erdoganladevam.herokuapp.com/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get('videoid')
@@ -30,19 +28,15 @@ def index(m3u8):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     ts = requests.get(source, headers=headers)
-    print(str(datetime.datetime.now()) +" - " + source + " istek cevap suresi: " + str(datetime.datetime.now() - now));
-
-    tsal = ts.text.replace(videoid+'_', f'https://cors-proxy.fringe.zone/https://hammerhead-app-bcw5r.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
+    tsal = ts.text.replace(videoid+'_', f'https://cors-proxy.fringe.zone/https://neset-baba-sigara.keremihsanozer.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/{videoid}_')
     if 'internal' in tsal:
-        tsal = tsal.replace('internal', f'https://cors-proxy.fringe.zone/https://hammerhead-app-bcw5r.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
+        tsal = tsal.replace('internal', f'https://cors-proxy.fringe.zone/https://neset-baba-sigara.keremihsanozer.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/internal')
     if 'segment' in tsal:
-        tsal = tsal.replace('\nmedia', f'\nhttps://cors-proxy.fringe.zone/https://hammerhead-app-bcw5r.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
+        tsal = tsal.replace('\nmedia', f'\nhttps://cors-proxy.fringe.zone/https://neset-baba-sigara.keremihsanozer.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
     return tsal
-
+ 
 @app.route('/getm3u8', methods=['GET'])
 def getm3u8():
-    now = datetime.datetime.now()
-
     source = request.args.get('source')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
@@ -61,20 +55,16 @@ def getm3u8():
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     ts = requests.get(source, headers=headers)
-    print(str(datetime.datetime.now()) +" - " + source + " istek cevap suresi: " + str(datetime.datetime.now() - now));
-
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://hammerhead-app-bcw5r.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    tsal = tsal.replace(videoid+'_','https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
-
+ 
 @app.route('/getstream',methods=['GET'])
 def getstream():
     param = request.args.get("param")
-    now = datetime.datetime.now()
-
     if param == "getts":
         source = request.url
-        source = source.replace('https://hammerhead-app-bcw5r.ondigitalocean.app/getstream?param=getts&source=','')
+        source = source.replace('https://erdoganladevam.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
@@ -92,15 +82,11 @@ def getstream():
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
         ts = requests.get(source,headers=headers)
-        print(str(datetime.datetime.now()) +" - " + source + " istek cevap suresi: " + str(datetime.datetime.now() - now));
-
         return ts.content
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
         r = requests.post("https://1xlite-2023337.top/cinema",json=veriler)
-        print(str(datetime.datetime.now()) +" - " + "1xlite" + " istek cevap suresi: " + str(datetime.datetime.now() - now));
-
         if "FullscreenAllowed" in r.text:
             veri = r.text
             veri = re.findall('"URL":"(.*?)"',veri)
@@ -116,10 +102,10 @@ def getstream():
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
             if "m3u8" in veri:
-                '''return "https://hammerhead-app-bcw5r.ondigitalocean.app/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://hammerhead-app-bcw5r.ondigitalocean.app/"+veri+'&videoid='+videoid
+                '''return "https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
+                return "https://erdoganladevam.herokuapp.com/"+veri+'&videoid='+videoid
         else:
             return "Veri yok"
-
+ 
 if __name__ == '__main__':
     app.run()
