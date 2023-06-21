a3f69524881cb374bc831630b67e75b6d9f2f01b
pesland <127784800+pesland@users.noreply.github.com>
2023-06-21 12:26:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 import re
-
+ 
 app = Flask(__name__)
 CORS(app)
-
+ 
 @app.route('/<m3u8>')
 def index(m3u8):
     m3u8 = request.url.replace('__', '/')
@@ -34,7 +34,7 @@ def index(m3u8):
     if 'segment' in tsal:
         tsal = tsal.replace('\nmedia', f'\nhttps://cors-proxy.fringe.zone/https://neset-baba-sigara.keremihsanozer.workers.dev/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/{videoid}/1/media')
     return tsal
-
+ 
 @app.route('/getm3u8', methods=['GET'])
 def getm3u8():
     source = request.args.get('source')
@@ -58,15 +58,15 @@ def getm3u8():
     tsal = ts.text
     tsal = tsal.replace(videoid+'_','https://erdoganladevam.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
-
-@app.route('/getstream', methods=['GET'])
+ 
+@app.route('/getstream',methods=['GET'])
 def getstream():
     param = request.args.get("param")
     if param == "getts":
         source = request.url
-        source = source.replace('https://erdoganladevam.herokuapp.com/getstream?param=getts&source=', '')
-        source = source.replace('%2F', '/')
-        source = source.replace('%3F', '?')
+        source = source.replace('https://erdoganladevam.herokuapp.com/getstream?param=getts&source=','')
+        source = source.replace('%2F','/')
+        source = source.replace('%3F','?')
         headers = {
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
@@ -81,16 +81,16 @@ def getstream():
             'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
-        ts = requests.get(source, headers=headers)
+        ts = requests.get(source,headers=headers)
         return ts.content
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://1xlite-2023337.top/cinema", json=veriler)
+        r = requests.post("https://1xlite-2023337.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
-            veri = re.findall('"URL":"(.*?)"', veri)
-            veri = veri[0].replace("\/", "__")
+            veri = re.findall('"URL":"(.*?)"',veri)
+            veri = veri[0].replace("\/", "/")
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge100','edge10')
             veri = veri.replace('edge4','edge10')
@@ -101,11 +101,11 @@ def getstream():
             veri = veri.replace('edge7', 'edge10')
             veri = veri.replace(':43434','')
             veri = veri.replace('edge100','edge10')
-            if "m3u8" in veri:
-                '''return "https://erdoganladevam.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://erdoganladevam.herokuapp.com/" + veri + '&videoid=' + videoid
-        else:
-            return "Veri yok"
-
+            veri = re.sub(r'https:\/{2,}','https://', veri)
+            veri = re.sub(r'edge\d+\.xmediaget\.com','edge10.xmediaget.com',veri)
+            veri = re.sub(r'edge\d+','edge10',veri)
+            veri = veri.replace('edge10.xmediaget.com/hls-live','erdoganladevam.herokuapp.com')
+            return veri
+ 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
