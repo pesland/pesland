dab57ac538f8241d1951d12a7624460d1f8cdbc5
pesland <127784800+pesland@users.noreply.github.com>
2024-01-20 11:12:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,18 @@ def index(m3u8):
     videoid = request.args.get("videoid")
     
     headers = {
-        # Your headers
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "tr-TR, tr;q = 0.9",
+        "origin": "https://www.maltinok.com",
+        "referer": "https://www.maltinok.com/",
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     
     ts = session.get(source, headers=headers)
@@ -43,7 +54,18 @@ def getm3u8():
     source = source.replace('%3F', '?')
     
     headers = {
-        # Your headers
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "tr-TR, tr;q = 0.9",
+        "origin": "https://www.maltinok.com",
+        "referer": "https://www.maltinok.com/",
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     
     ts = session.get(source, headers=headers)
@@ -64,7 +86,18 @@ def getstream():
         source = source.replace('%3F', '?')
         
         headers = {
-            # Your headers
+            "accept": "*/*",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "tr-TR, tr;q = 0.9",
+            "origin": "https://www.maltinok.com",
+            "referer": "https://www.maltinok.com/",
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
         
         ts = session.get(source, headers=headers)
