73eff5d8c2c07617f9112977affcf60fce2ae398
pesland <127784800+pesland@users.noreply.github.com>
2024-01-20 11:11:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
-import grequests
-from flask import Flask, request
-from flask_caching import Cache
+import requests
+from flask import Flask, request, Response
+from flask_cors import CORS
 import re
-import concurrent.futures
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-
 CORS(app)
 
-def make_request(session, url, headers):
-    return session.get(url, headers=headers)
-
-@cache.cached(timeout=60)  # 60 saniye önbellekte sakla
-def get_data_from_source(url, headers):
-    ts = requests.get(url, headers=headers)
-    return ts.text
-
-def make_async_requests(urls, headers):
-    rs = (grequests.get(u, headers=headers) for u in urls)
-    responses = grequests.map(rs)
-    return responses[0]
-
-def make_parallel_request(url, headers):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(make_request, url, headers)]
-        return futures[0].result()
+# Use a global session for making HTTP requests
+session = requests.Session()
 
 @app.route('/<m3u8>')
 def index(m3u8):
@@ -34,28 +16,18 @@ def index(m3u8):
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get("videoid")
-
+    
     headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q=0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        # Your headers
     }
+    
+    ts = session.get(source, headers=headers)
+    tsal = ts.text
 
-    tsal = get_data_from_source(source, headers)
     tsal = tsal.replace(videoid + '_', 'https://worker-holy-shadow-d82a.volestream--geldi-kizlarr.workers.dev/https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/' + videoid + '/1/' + videoid + '_')
 
     if "internal" in tsal:
-        tsal = tsal.replace('internal',
-                            'https://worker-holy-shadow-d82a.volestream--geldi-kizlarr.workers.dev/https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/' + videoid + '/1/internal')
+        tsal = tsal.replace('internal', 'https://worker-holy-shadow-d82a.volestream--geldi-kizlarr.workers.dev/https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/' + videoid + '/1/internal')
 
     if "segment" in tsal:
         tsal = tsal.replace('\n' + 'media',
@@ -63,80 +35,59 @@ def index(m3u8):
 
     return tsal
 
+
 @app.route('/getm3u8', methods=['GET'])
 def getm3u8():
     source = request.url.replace('https://seashell-app-cdk6o.ondigitalocean.app/getm3u8?source=', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
+    
     headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q=0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        # Your headers
     }
-
-    ts = make_parallel_request(source, headers)
+    
+    ts = session.get(source, headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid + '_',
-                        'https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/' + videoid + '/1/' + videoid + '_')
-
+    videoid = request.args.get("videoid")
+    tsal = tsal.replace(videoid + '_', 'https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/' + videoid + '/1/' + videoid + '_')
+    
     return tsal
+
 
 @app.route('/getstream', methods=['GET'])
 def getstream():
     param = request.args.get("param")
+
     if param == "getts":
         source = request.url.replace('https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=', '')
         source = source.replace('%2F', '/')
         source = source.replace('%3F', '?')
+        
         headers = {
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'tr-TR,tr;q=0.9',
-            'origin': 'https://www.maltinok.com',
-            'referer': 'https://www.maltinok.com/',
-            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+            # Your headers
         }
-
-        ts = make_parallel_request(source, headers)
-        return ts.content
-
+        
+        ts = session.get(source, headers=headers)
+        return Response(ts.iter_content(chunk_size=128), content_type=ts.headers['content-type'])
+    
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://melbet-17181.top/cinema", json=veriler)
+        r = session.post("https://melbet-17181.top/cinema", json=veriler)
+
         if "FullscreenAllowed" in r.text:
-            veri = r.text
-            veri = re.findall('"URL":"(.*?)"', veri)
-            veri = veri[0].replace("\/", "__")
-            veri = veri.replace('edge3', 'edge10')
-            veri = veri.replace('edge100', 'edge10')
-            veri = veri.replace('edge4', 'edge10')
-            veri = veri.replace('edge2', 'edge10')
-            veri = veri.replace('edge5', 'edge10')
-            veri = veri.replace('edge1', 'edge10')
-            veri = veri.replace('edge6', 'edge10')
-            veri = veri.replace('edge7', 'edge10')
-            veri = veri.replace(':43434', '')
-            veri = veri.replace('edge100', 'edge10')
+            veri = re.findall('"URL":"(.*?)"', r.text)[0].replace("\/", "__")
+            veri = veri.replace('edge3', 'edge10').replace('edge100', 'edge10').replace('edge4', 'edge10').replace(
+                'edge2', 'edge10').replace('edge5', 'edge10').replace('edge1', 'edge10').replace('edge6',
+                                                                                                   'edge10').replace(
+                'edge7', 'edge10').replace(':43434', '').replace('edge100', 'edge10')
+
             if "m3u8" in veri:
                 return "https://seashell-app-cdk6o.ondigitalocean.app/" + veri + '&videoid=' + videoid
+
         else:
             return "Veri yok"
+
 
 if __name__ == '__main__':
     app.run()
