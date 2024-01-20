fe41a1604d0af56a9bcd0710abf5cd91545b7b51
pesland <127784800+pesland@users.noreply.github.com>
2024-01-20 10:38:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
-import requests
-from flask import Flask
-from flask import request
-from flask_cors import CORS
 import re
+from flask import Flask, Response, request
+from flask_cors import CORS
+import requests
+
 app = Flask(__name__)
 CORS(app)
+
+def generate_ts_content(source_url, headers):
+    ts = requests.get(source_url, headers=headers, stream=True)
+    for chunk in ts.iter_content(chunk_size=1024):
+        if chunk:
+            yield chunk
+
 @app.route('/<m3u8>')
 def index(m3u8):
-    m3u8 = request.url.replace('__','/')
-    source = m3u8
+    source = request.url.replace('__', '/')
     source = source.replace('https://seashell-app-cdk6o.ondigitalocean.app/', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get("videoid")
-    '''source = source.replace(videoid+'.m3u8',videoid)'''
+
     headers = {
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
+        "accept-language": "tr-TR, tr;q=0.9",
         "origin": "https://www.maltinok.com",
         "referer": "https://www.maltinok.com/",
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
@@ -28,25 +34,18 @@ def index(m3u8):
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    ts = requests.get(source, headers=headers)
-    tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://worker-holy-shadow-d82a.volestream--geldi-kizlarr.workers.dev/https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
-    if "internal" in tsal:
-        tsal = tsal.replace('internal','https://worker-holy-shadow-d82a.volestream--geldi-kizlarr.workers.dev/https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/internal')
-    if "segment" in tsal:
-        tsal = tsal.replace('\n'+'media','\n'+'https://worker-holy-shadow-d82a.volestream--geldi-kizlarr.workers.dev/https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/media')
-    return tsal
 
-@app.route('/getm3u8',methods=['GET'])
+    return Response(generate_ts_content(source, headers), content_type='video/mp2t')
+
+@app.route('/getm3u8', methods=['GET'])
 def getm3u8():
-    source = request.url
-    source = source.replace('https://seashell-app-cdk6o.ondigitalocean.app/getm3u8?source=', '')
+    source = request.url.replace('https://seashell-app-cdk6o.ondigitalocean.app/getm3u8?source=', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     headers = {
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
+        "accept-language": "tr-TR, tr;q=0.9",
         "origin": "https://www.maltinok.com",
         "referer": "https://www.maltinok.com/",
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
@@ -57,19 +56,17 @@ def getm3u8():
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    ts = requests.get(source, headers=headers)
-    tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
-    return tsal
 
-@app.route('/getstream',methods=['GET'])
+    return Response(generate_ts_content(source, headers), content_type='video/mp2t')
+
+@app.route('/getstream', methods=['GET'])
 def getstream():
     param = request.args.get("param")
+
     if param == "getts":
-        source = request.url
-        source = source.replace('https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=','')
-        source = source.replace('%2F','/')
-        source = source.replace('%3F','?')
+        source = request.url.replace('https://seashell-app-cdk6o.ondigitalocean.app/getstream?param=getts&source=', '')
+        source = source.replace('%2F', '/')
+        source = source.replace('%3F', '?')
         headers = {
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
@@ -84,29 +81,30 @@ def getstream():
             'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
-        ts = requests.get(source,headers=headers)
-        return ts.content
+
+        return Response(generate_ts_content(source, headers), content_type='video/mp2t')
+
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://melbet-17181.top/cinema",json=veriler)
+        r = requests.post("https://melbet-17181.top/cinema", json=veriler)
+
         if "FullscreenAllowed" in r.text:
-            veri = r.text
-            veri = re.findall('"URL":"(.*?)"',veri)
+            veri = re.findall('"URL":"(.*?)"', r.text)
             veri = veri[0].replace("\/", "__")
-            veri = veri.replace('edge3','edge10')
-            veri = veri.replace('edge100','edge10')
-            veri = veri.replace('edge4','edge10')
-            veri = veri.replace('edge2','edge10')
-            veri = veri.replace('edge5','edge10')
-            veri = veri.replace('edge1','edge10')
+            veri = veri.replace('edge3', 'edge10')
+            veri = veri.replace('edge100', 'edge10')
+            veri = veri.replace('edge4', 'edge10')
+            veri = veri.replace('edge2', 'edge10')
+            veri = veri.replace('edge5', 'edge10')
+            veri = veri.replace('edge1', 'edge10')
             veri = veri.replace('edge6', 'edge10')
             veri = veri.replace('edge7', 'edge10')
-            veri = veri.replace(':43434','')
-            veri = veri.replace('edge100','edge10')
+            veri = veri.replace(':43434', '')
+            veri = veri.replace('edge100', 'edge10')
+
             if "m3u8" in veri:
-                '''return "https://seashell-app-cdk6o.ondigitalocean.app/getm3u8?source="+veri+'&videoid='+videoid'''
-                return "https://seashell-app-cdk6o.ondigitalocean.app/"+veri+'&videoid='+videoid
+                return "https://seashell-app-cdk6o.ondigitalocean.app/" + veri + '&videoid=' + videoid
         else:
             return "Veri yok"
 
